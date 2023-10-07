cd4a3af73839c0de908a52f01ad1ce245540cef1
Atharv Garg <mark.atharv@gmail.com>
2023-10-07 14:58:23 +0530

    Topbar initial styling

--- dashboard/src/components/Topbar.tsx
+++ dashboard/src/components/Topbar.tsx
@@ -2,8 +2,17 @@
 
 const Topbar = () => {
     return (
-        <nav>
-            <div className=" min-w-max h-12 bg-zinc-900 ">
+        <nav className=" bg-zinc-900 z-10 min-w-max w-screen h-12">
+            <div className=" container mx-0 flex flex-wrap p-4 w-screen flex-col">
+                <div className="flex flex-wrap text-base justify-center w-screen">
+                    <a className="flex font-medium text-left text-gray-100 mb-4">
+                        <img src="./public/logo192.png" alt="logo"/>
+                    </a>
+                    <a className="mr-5">First Link</a>
+                    <a className="mr-5">Second Link</a>
+                    <a className="mr-5">Third Link</a>
+                    <a className="mr-5">Fourth Link</a>
+                </div>
 
             </div>
         </nav>
--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '9': 'repeat(9, minmax(1, 1fr)) auto',
+      }
+    },
   },
   plugins: [],
 }
