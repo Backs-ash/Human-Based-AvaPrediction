5f33117ac2faf16ce6e2663917bbff999a922cda
Atharv Garg <mark.atharv@gmail.com>
2023-10-08 10:40:51 +0530

    Sidebar upload

--- dashboard/src/components/sidebar.tsx
+++ dashboard/src/components/sidebar.tsx
@@ -1,7 +1,23 @@
 
-const Sidebar = () => {
+
+const Sidebar: React.FC = () => {
+    const profileData = [
+        { name: 'User 1', bio: 'Bio 1' },
+        { name: 'User 2', bio: 'Bio 2' },
+    ];
+
     return (
-        <div></div>
+        <div className="flex">
+            {/* Scrollable profile container */}
+            <div className="container z-[2] mt-12 w-1/4 p-4 overflow-y-scroll">
+                {profileData.map((profile, index) => (
+                <div key={index} className=" bg-zinc-700 rounded-lg shadow-lg p-4 mb-4">
+                    <h2 className="text-xl bg-zinc-700 font-semibold">{profile.name}</h2>
+                    <p className="text-gray-400 bg-zinc-700">{profile.bio}</p>
+                </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
