adddce741fdc098f244ca08dd9f1b84524183194
Swastick-Paliwal <paliwal.swastick@gmail.com>
2023-10-08 17:31:24 +0530

    sidbar working and react leafelet addons created

--- dashboard/src/components/MainBody.tsx
+++ dashboard/src/components/MainBody.tsx
@@ -1,9 +1,9 @@
-
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
 
 export default function MainBody() {
     return (
         <div>
-             
+            
         </div>
     )
 }
--- dashboard/src/components/sidebar.tsx
+++ dashboard/src/components/sidebar.tsx
@@ -1,19 +1,38 @@
-
+import pfpImage from '../assets/pfp.png';
+import {
+    IconHeartbeat,
+    IconLungs,
+    IconTemperature,
+    IconDropletHeart,
+} from "@tabler/icons-react";
 
 const Sidebar: React.FC = () => {
     const profileData = [
-        { name: 'User 1', bio: 'Bio 1' },
-        { name: 'User 2', bio: 'Bio 2' },
+        { name: 'User 1', bio1: '69bpm', bio2: '11b/m', bio3: '36.9C', bio4: '96%'},
+        { name: 'User 2', bio1: '69bpm', bio2: '11b/m', bio3: '36.9C', bio4: '96%'},
     ];
 
     return (
         <div className="flex">
             {/* Scrollable profile container */}
-            <div className="container z-[2] mt-12 w-1/4 p-4 overflow-y-scroll">
+            <div className="container z-[2] mt-12 w-1/4 p-2 overflow-y-scroll">
                 {profileData.map((profile, index) => (
-                <div key={index} className=" bg-zinc-700 rounded-lg shadow-lg p-4 mb-4">
-                    <h2 className="text-xl bg-zinc-700 font-semibold">{profile.name}</h2>
-                    <p className="text-gray-400 bg-zinc-700">{profile.bio}</p>
+                <div key={index} className="bg-zinc-700 rounded-lg shadow-lg h-1/3 p-2 mb-4">
+                    <button className="grid grid-cols-2 p-0 h-full w-full bg-inherit">
+                        <div className="rounded-full overflow-hidden w-12 h-12">
+                            <img src={ pfpImage } className="w-full h-full object-cover" alt={profile.name} />
+                        </div>
+                        <div className="grid grid-cols-4 p-0 w-full h-full bg-inherit">
+                        <IconHeartbeat className=" inline-flex object-cover bg-zinc-700" />
+                        <p className="text-gray-400 bg-zinc-700">{profile.bio1}</p>
+                        <IconLungs className=" inline-flex object-cover bg-zinc-700" />
+                        <p className="text-gray-400 bg-zinc-700">{profile.bio2}</p>
+                        <IconTemperature className=" inline-flex object-cover bg-zinc-700" />
+                        <p className="text-gray-400 bg-zinc-700">{profile.bio3}</p>
+                        <IconDropletHeart className=" inline-flex object-cover bg-zinc-700" />
+                        <p className="text-gray-400 bg-zinc-700">{profile.bio4}</p>
+                        </div>
+                    </button>
                 </div>
                 ))}
             </div>
